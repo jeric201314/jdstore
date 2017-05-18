ff5576844e1c3efd039dfe4b5c26593b15751b27
hjeric201314 <hjeric201314@gmail.com>
2017-05-18 22:37:32 +0800

    implement image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518140656) do
+ActiveRecord::Schema.define(version: 20170518142247) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170518140656) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|

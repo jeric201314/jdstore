fef3a08aa7be89d00ab27c4e535f21926b3f74fa
hjeric201314 <hjeric201314@gmail.com>
2017-05-18 23:58:22 +0800

    change admin/product backed panel

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170518142247) do
 
-  create_table "posts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "group_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"

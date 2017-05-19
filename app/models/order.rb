f427ca96d524ea5fe884b3113340e1196441a4ed
hjeric201314 <hjeric201314@gmail.com>
2017-05-19 23:20:36 +0800

    add aasm_state to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -32,7 +32,8 @@ class Order < ApplicationRecord
     state :order_cancelled
     state :good_returned
 
-    event :make_payment,after_commit: :pay! do
+
+    event :make_payment do
       transitions from: :order_placed, to: :paid
     end
 
@@ -45,7 +46,7 @@ class Order < ApplicationRecord
     end
 
     event :return_good do
-      transitions from: :shipped,     to: :good_returned
+      transitions from: :shipped,      to: :good_returned
     end
 
     event :cancel_order do
